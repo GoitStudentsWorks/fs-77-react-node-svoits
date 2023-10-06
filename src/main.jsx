c7d06758c06c17dacb76c14158fd894ab4686c19
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-10-06 13:54:14 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/drink-master-frontend/">
+        <BrowserRouter basename="/fs-77-react-node-svoits/">
           <App />
         </BrowserRouter>
       </PersistGate>
